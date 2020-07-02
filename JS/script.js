37683a0a1186f65fcc4d06f231fdf172d468d849
Andryk Lara <lara.cordero03@gmail.com>
2020-07-02 14:38:48 -0600

    Practica 4 ejercicio

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,10 @@ let numero = parseFloat(prompt("Digite un numero"));
 
 //Promedio
 
-while (numero) {
+while (numero != cero) { //No guarda el primer numero, buscar solucion. Buscar la forma del string vacio.
     numero = parseFloat(prompt('Digite un numero'));
-    numeros.push(numero);    
+    numeros.push(numero);   
+    
 }
 numeros.sort((a, b) => a - b);
 
@@ -19,17 +20,15 @@ for (let i = 0; i < numeros.length; i++) {
     
 console.log(`El promedio es: ${promedio / numeros.length}`);
 
-//Mediana
+//Mediana del array
 
 let medianaAlta = parseInt((numeros.length / 2) + 0.6);
 let medianaBaja = parseInt((numeros.length / 2) - 0.6);
 
+let medianaAltafinal = numeros[medianaBaja]
+let medianaBajafinal = numeros[medianaAlta]
 
-if (numeros.length % 2 == 0) {
-    console.log(`La mediana es: ${parseFloat(numeros[medianaAlta])}`);
-} else {
-    console.log(`La mediana es: ${(numeros[parseFloat(medianaAlta)] + numeros[parseFloat(medianaBaja)]) / 2}`);
-}
+console.log(`La mediana es: ${(medianaBajafinal + medianaAltafinal) / 2}`)
 
 //Valor Minimo y Valor Maximo
 
